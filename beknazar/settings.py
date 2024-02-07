759339e80180335b07777709ec7cfc60913037ed
Soltanbkz <soltanbkzbek@gmail.com>
2024-02-07 23:48:38 +0600

    chane

--- beknazar/settings.py
+++ beknazar/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'beknazar/main/static/'
+STATIC_ROOT = BASE_DIR / 'main/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "/main/static/",
